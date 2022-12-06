b50ae4477cfe0ec2e65fdf895fc5cd4d137b1d33
Darius1182000 <merca_darius@yahoo.ro>
2022-12-06 18:38:31 +0200

    laboratorul8

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using LibraryModel.Data;
 using Microsoft.EntityFrameworkCore;
 using Merca_Darius_Lab2.Data;
+using Merca_Darius_Lab2.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<LibraryContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSignalR();
 //
 
 var app = builder.Build();
@@ -36,5 +38,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/Chat");
 
 app.Run();
